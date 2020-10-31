e6076d3ea199dd95e28d51c4cebb71e932f9a50d
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-31 10:46:11 +0100

    Pass by reference

--- Project/Project/Source1.cpp
+++ Project/Project/Source1.cpp
@@ -2,23 +2,32 @@
 #include <array>
 #include <vector>
 
+//void print_contents(std::vector<int>* input) {
+//	for (int i = 0; i < input->size(); i++) {
+//		std::cout << input->at(i) << std::endl;
+//	}
+//}
+
+void print_contents(std::vector<int>& input) {
+	for (int i = 0; i < input.size(); i++) {
+		std::cout << input [i] << std::endl;
+	}
+}
+
 int main() {
 
-	std::array<int,4> numbers;
+	int size;
 
-	for (int i = 0; i < 4; i++) {
-		numbers[i] = i;
+	std::cout << "Provide a size for your array: " << std::endl;
+	std::cin >> size;
+
+	std::vector<int> numbers;
+
+	for (int i = 0; i < size; i++) {
+		numbers.push_back(i);
 	}
 
-	//int step_size = &numbers[3] - &numbers[0];
-	//std::cout << step_size << std::endl;
-
-	int* ptr_numbers = &numbers[0];
-
-	for (int i = 0; i < 4; i++) {
-		std::cout << *ptr_numbers << std::endl;
-		++ptr_numbers;
-	}
+	print_contents(numbers);
 
 	return 0;
 }

4477a8fd235620a973b441b767a975fd0a523d8b
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-31 09:56:21 +0100

    First pointer

--- Project/Project/Source1.cpp
+++ Project/Project/Source1.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+#include <array>
+#include <vector>
+
 int main() {
+
+	int variable = 7;
+
+	int* ptr;
+
+	std::cout << ptr << std::endl;
+
+	ptr = &variable;
+	std::cout << *ptr << std::endl;
+	*ptr = 25;
+	std::cout << variable << std::endl;
+
 	return 0;
 }
